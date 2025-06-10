20c98bfc32fb01bd069a4d0d9d02a771c423ef9d
ChunTeng <chunteng9674@gmail.com>
2025-06-10 16:47:07 +0800

    setup (incomplete)

---
+++ resources/views/admin/layout/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('page_title') - I Mum Mum</title>
+
+    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+</head>
+
+<body>
+    @include('admin/layout/side-bar')
+
+    <div class="main">
+        @include('admin/layout/top-bar')
+
+        @yield('content')
+    </div>
+</body>
+
+</html>
