e177add37bf994ebe80468faf7f26e8dadd8f42b
hxyj12 <hxhanmeng@gmail.com>
2025-06-11 14:50:44 +0800

    change the setup,user profile make it to the right

--- resources/views/admin/layout/top-bar.blade.php
+++ resources/views/admin/layout/top-bar.blade.php
@@ -1,33 +1,24 @@
-<!-- 导航栏 Top Bar -->
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top custom-navbar">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">I Mum Mum 板面专卖店</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <!-- Sidebar Trigger Button 可放左边（留空也可以）-->
+        <button class="navbar-toggler me-2" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle sidebar">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">库存管理</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">低库存</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">订单录入</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        用户
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">账户设置</a></li>
-                        <li><a class="dropdown-item" href="#">退出</a></li>
-                    </ul>
-                </li>
-            </ul>
+
+        <!-- 店名 Brand -->
+        <a class="navbar-brand me-auto" href="#">I Mum Mum 板面专卖店</a>
+
+        <!-- 用户资料区域：在手机时靠右 -->
+        <div class="d-flex ms-auto">
+            <div class="dropdown">
+                <a class="nav-link dropdown-toggle text-white" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    用户
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                    <li><a class="dropdown-item" href="#">账户设置</a></li>
+                    <li><a class="dropdown-item" href="#">退出</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </nav>
