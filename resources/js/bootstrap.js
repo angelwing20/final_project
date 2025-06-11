d0ba72a2ae1b8df2f1936214a1bc8fce2cb28878
ChunTeng <chunteng9674@gmail.com>
2025-06-11 08:03:40 +0800

    Revert "setup (incomplete)"
    This reverts commit 20c98bfc32fb01bd069a4d0d9d02a771c423ef9d.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,11 +2,3 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-import 'bootstrap';
-
-import $ from 'jquery';
-window.$ = window.jQuery = $;
-
-import SimpleBar from "simplebar";
-window.SimpleBar = SimpleBar;
