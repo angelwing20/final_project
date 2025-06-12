66154ac9629e572759e435653b4379238cce732b
Gg040805 <dennis1204205@gmail.com>
2025-06-12 10:54:59 +0800

    Reapply "Sidebar"
    This reverts commit fd7b3b3ebd821b3a8b3aab4c6e1a9a1b4cb967cb.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,10 @@ import "select2"; // JS 功能（让你可以用 $('.select2').select2()）
 import "select2/dist/css/select2.min.css"; // 样式
 import "select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.min.css"; // bootstrap 5 的主题样式
 
+import 'bootstrap';
+import '@fortawesome/fontawesome-free/js/all.min.js';
+
+
 // window.resetForm = function (targetForm) {
 //     $(targetForm)[0].reset();
 //     $(targetForm).eq(0).find("select").val("").trigger("change");
--- resources/views/admin/layout/side-bar.blade.php
+++ resources/views/admin/layout/side-bar.blade.php
@@ -1,12 +1,64 @@
-<nav id="sidebar">
-    <div class="logo-container">
-        <img class="logo-image" src="" alt="">
-    </div>
-    <div class="scrollable-area" data-simplebar>
-        <div class="sidebar-nav">
-            <a class="sidebar-link" href="">
+<head>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="https://kit.fontawesome.com/your-kit-id.js" crossorigin="anonymous"></script>
+</head>
+<!-- Menu Button for Small Screens -->
+<button class="btn btn-outline-primary d-md-none m-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="sidebar">
+    <i class="fas fa-bars"></i> Menu
+</button>
 
-            </a>
-        </div>
+<div class="sidebar" id="sidebar">
+    <br><br>
+    <!-- Logo Section -->
+    <div class="logo-container">
+        <img src="{{ asset('img/i_mum_mum_logo.png') }}" alt="Logo" class="logo-image">
     </div>
-</nav>
+
+    <!-- Scrollable Nav Area -->
+    <div class="scrollable-area">
+        <nav class="sidebar-nav">
+            <!-- Top Links -->
+            <a href="#" class="sidebar-link ">
+                <i class="fas fa-chart-line"></i>
+                Dashboard
+            </a>
+
+            <!-- Collapsible Ingredients Section -->
+            <a class="sidebar-link d-flex justify-content-between align-items-center" data-bs-toggle="collapse" href="#ingredientsCollapse" role="button" aria-expanded="false" aria-controls="ingredientsCollapse">
+                <span><i class="fas fa-carrot"></i> Ingredients</span>
+                <i class="fas fa-angle-down"></i>
+            </a>
+
+            <div class="collapse ps-4" id="ingredientsCollapse">
+                <a href="#" class="sidebar-link">
+                    <i class="far fa-circle"></i> Vegetables
+                </a>
+              
+                <a href="#" class="sidebar-link">
+                    <i class="far fa-dot-circle" style="color: lightgreen;"></i> Fruits
+                </a>
+            </div>
+
+            <a href="#" class="sidebar-link">
+                <i class="fas fa-boxes-stacked"></i>
+                Inventory
+            </a>
+
+            <a href="#" class="sidebar-link">
+                <i class="fas fa-truck"></i>
+                Suppliers
+            </a>
+
+            <!-- Divider -->
+            <div class="sidebar-divider"></div>
+
+            <!-- Category Group -->
+            <a href="#" class="sidebar-link">
+                <i class="fas fa-layer-group"></i>
+                Categories
+            </a>
+
+            
+        </nav>
+    </div>
+</div>
