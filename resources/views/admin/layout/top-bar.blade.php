f92edcac58a20f0e8b70c6553b352cd01afc818b
hxyj12 <hxhanmeng@gmail.com>
2025-06-12 09:10:51 +0800

    fix the profile not show

--- resources/views/admin/layout/top-bar.blade.php
+++ resources/views/admin/layout/top-bar.blade.php
@@ -1,27 +1,27 @@
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top custom-navbar">
     <div class="container-fluid">
-        <!-- Sidebar Trigger Button 可放左边（留空也可以）-->
+        <!-- Sidebar Trigger Button -->
         <button class="navbar-toggler me-2" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle sidebar">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <!-- 店名 Brand -->
+        <!-- 品牌 -->
         <a class="navbar-brand me-auto" href="#">I Mum Mum 板面专卖店</a>
 
-        <!-- 用户资料区域：在手机时靠右 -->
-        <div class="d-flex ms-auto">
-            <div class="dropdown">
-                <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="images/default-avatar-dark.png" class="user-image"
-                    onerror="this.onerror=null; this.src='images/default-avatar-dark.png'">
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
-                    @auth
-                        <li><a class="dropdown-item" href="#">账户设置</a></li>
-                        <li><a class="dropdown-item" href="{{ route('logout') }}">退出</a></li>
-                    @endauth
-                </ul>
-            </div>
+        <!-- 用户头像 dropdown -->
+        <div class="dropdown ms-auto">
+            <a href="#" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="{{ asset('img/default-avatar-dark.png') }}" class="user-image" onerror="this.onerror=null; this.src='img/default-avatar-dark.png'">
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                <li><a class="dropdown-item" href="#">账户设置</a></li>
+                <li>
+                    <form id="logout-form" action="" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                    <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">退出</a>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
